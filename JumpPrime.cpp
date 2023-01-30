4bcc0eb6b6c2f20caf70d753717381aed109eebb
profounddark <39137591+profounddark@users.noreply.github.com>
2023-01-29 20:32:10 -0800

    moved comments to .h, updated front matter

--- JumpPrime.cpp
+++ JumpPrime.cpp
@@ -4,13 +4,7 @@
 
 #include "JumpPrime.h"
 
-/**
- * isPrime determines whether or not the given positive integer is a prime
- * number or not (i.e., a whole number greater than one that cannot be exactly
- * divided by any whole number other than itself)
- * @param testNumber the positive integer to test
- * @return true if the number is prime, false otherwise
- */
+
 bool JumpPrime::isPrime(unsigned int testNumber) {
 
     for (unsigned int i = 2; i < testNumber; i++) {
@@ -22,15 +16,6 @@ bool JumpPrime::isPrime(unsigned int testNumber) {
     return true;
 }
 
-/**
- * findPrime finds either the next nearest prime number or the previous nearest
- * prime number in sequence, depending on the value of the passed parameter.
- * @param startValue the positive integer to start the search from
- * @param findNext true to return the next prime number in sequence, false to
- * return the previous prime number in sequence.
- * @return the next (or previous) positive prime integer within the bounds of
- * the unsigned integer date type
- */
 unsigned int JumpPrime::findPrime(unsigned int startValue, bool findNext) {
     // determine if this needs to count up or down
     int stepValue = findNext ? 1 : -1;
@@ -44,11 +29,6 @@ unsigned int JumpPrime::findPrime(unsigned int startValue, bool findNext) {
     return result;
 }
 
-
-/**
- * setPrimeLimits finds a new upper and lower prime number based on the
- * established stored number (mainNumber).
- */
 void JumpPrime::setPrimeLimits() {
 
     upperPrime = findPrime(mainNumber, true);
@@ -56,21 +36,11 @@ void JumpPrime::setPrimeLimits() {
 
 }
 
-/**
- * resetQueryCounter sets the new query limits (based on the distance between
- * the next and previosu prime number) and resets the query counter to 0.
- */
 void JumpPrime::resetQueryCounter() {
     queryLimit = upperPrime - lowerPrime;
     queryCount = 0;
 }
 
-/**
- * jumpNumber "jumps" the value of the stored number, mainNumber, by a
- * specified amount. After a set number of "jumps", the JumpPrime will deactive.
- * @param jumpValue the value (positive or negative) to "jump" the stored
- * number by.
- */
 void JumpPrime::jumpNumber(int jumpValue) {
 
     // initiate the jump
@@ -105,14 +75,6 @@ JumpPrime::JumpPrime(unsigned int initValue, unsigned int jumpBound) {
     }
 }
 
-/**
- * up returns the next highest prime number from the number stored in the
- * JumpPrime object. Does not return accurate results if the JumpPrime object
- * has been deactivated.
- * PRECONDITION: the JumpPrime object is running.
- * @return the next highest prime number. If the JumpPrime object has been
- * deactivated, returns 0.
- */
 unsigned int JumpPrime::up() {
     if (currentState == Active) {
         // storing the upper prime in the case that the object jumps
@@ -131,15 +93,7 @@ unsigned int JumpPrime::up() {
     return 0;
 }
 
-/**
- * down returns the next lowest prime number from the number stored in the
- * JumpPrime object. Does not return accurate results if the JumpPrime object
- * has been deactivated.
- * PRECONDITION: the JumpPrime object is running.
- * @return the next lowest prime number. If the JumpPrime object has been
- * deactivated, returns 0.
- * @return
- */
+
 unsigned int JumpPrime::down() {
     if (currentState == Active) {
         // storing the upper prime in the case that the object jumps
@@ -158,12 +112,7 @@ unsigned int JumpPrime::down() {
 }
 
 
-/**
- * Reset attempts to reset the JumpPrime object to the original integer value.
- * This will fail if the JumpPrime object was already made irreparable.
- * PRECONDITION: the JumpPrime object is not permanently deactivated.
- * @return true if the reset is successful, false otherwise.
- */
+
 bool JumpPrime::reset() {
     if (currentState == Failed) {
         return false;
@@ -182,15 +131,7 @@ bool JumpPrime::reset() {
     }
 }
 
-/**
- * Revive attempts to revive a disabled JumpPrime object. This resets the
- * query and jump count and allows the object to be queried again. If the
- * object is already active, it permanently disables the object (makes it
- * irreparable).
- * PRECONDITION: the JumpPrime object is not currently active.
- * @return true if the attempt to revive the object was successful. false
- * otherwise.
- */
+
 bool JumpPrime::revive() {
     // object is not running and is not permanently broken
     if (currentState == Inactive) {
@@ -208,31 +149,16 @@ bool JumpPrime::revive() {
     return (currentState == Active);
 }
 
-/**
- * IsActive returns whether or not the JumpPrime object has been deactivated
- * due to excessive jumps.
- * @return true if currently active, false otherwise
- */
+
 bool JumpPrime::isActive() {
     return (currentState == Active);
 }
 
-/**
- * IsDeactivated returns whether or not the JumpPrime object has been
- * permanently deactivated due to improper operation.
- * @return true if the JumpPrime object has been permanently disabled,
- * false otherwise.
- */
+
 bool JumpPrime::isDisabled() {
     return (currentState == Failed);
 }
 
-/**
- * GetCurrentValue returns the current value encapsulated by the JumpPrime
- * object. When the object is instantiated (or reset), this is the initial
- * set value. After a jump, this is the new jumped-to value.
- * @return the current value encapsulated by the JumpPrime object.
- */
 unsigned int JumpPrime::getCurrentValue() {
     return mainNumber;
 }
--- JumpPrime.h
+++ JumpPrime.h
@@ -6,19 +6,32 @@
 #define INC_5011_P2_JUMPPRIME_H
 
 /**
- * The JumpPrime> object encapsulates a positive integer that must be at
- * least 4 digits long. The user can query for the object for the two nearest
+ * The JumpPrime object encapsulates a positive integer that must be at
+ * least 4 digits long. The user can query the object for the two nearest
  * Prime numbers (either higher or lower). After a set number of queries (based
  * on the distance between the base integer and the farther of the two nearest
  * primes), the JumpPrime object will "jump" to a new base number a set
  * distance from the original number. New queries as to the nearest prime
  * number will be based on this new number.
  *
- * After a set number of "jumps", the <c>JumpPrime</c> object will deactivate.
+ * After a set number of "jumps", the JumpPrime object will deactivate.
  * It can be reactivated resetting the base number to the original encapsulated
  * positive integer.
  *
- * DESIGN ASSUMPTIONS:
+ * METHODS:
+ * 1. up() and down() are the primary methods of use. They return the next or
+ * previous prime number in sequence (respectively).
+ * 2. revive() reactivates an object that has been deactivated due to
+ * exceeding the jump limit. An attempt to revive an object in any other
+ * state will result in the object failing.
+ * 3. reset() resets the object to its initial state. A failed object
+ * cannot be reset.
+ * 4. isActive() indicates whether the object is currently in the
+ * active state (i.e., capable of returning input).
+ * 5. isDisabled() indicates whether the object has failed. A failed object
+ * cannot be successfully queried nor can it be reset or revived.
+ *
+ * OTHER ASSUMPTIONS:
  * 1. When the JumpPrime object jumps, it jumps in the direction of the last
  * query. If it jumps after an `up` call, the jump is positive in direction.
  * If it jumps after a 'down' call, the jump is negative in direction.
@@ -30,7 +43,9 @@
  */
 class JumpPrime {
 
-    enum Status { Active, Inactive, Failed};
+    enum Status {
+        Active, Inactive, Failed
+    };
 
     // class constants
     static const unsigned int DEFAULT_JUMP_BOUND = 10;
@@ -52,28 +67,122 @@ class JumpPrime {
     unsigned int upperPrime;
     unsigned int lowerPrime;
 
+    /**
+     * isPrime determines whether or not the given positive integer is a prime
+     * number or not (i.e., a whole number greater than one that cannot be
+     * exactly divided by any whole number other than itself)
+     * @param testNumber the positive integer to test
+     * @return true if the number is prime, false otherwise
+     */
     bool isPrime(unsigned int testNumber);
+
+    /**
+     * findPrime finds either the next nearest prime number or the previous
+     * nearest prime number in sequence, depending on the value of the passed
+     * parameter.
+     * @param startValue the positive integer to start the search from
+     * @param findNext true to return the next prime number in sequence, false
+     * to return the previous prime number in sequence.
+     * @return the next (or previous) positive prime integer within the bounds
+     * of the unsigned integer date type
+     */
     unsigned int findPrime(unsigned int startValue, bool findNext);
+
+    /**
+     * setPrimeLimits finds a new upper and lower prime number based on the
+     * established stored number (mainNumber).
+     */
     void setPrimeLimits();
+
+    /**
+     * resetQueryCounter sets the new query limits (based on the distance between
+     * the next and previosu prime number) and resets the query counter to 0.
+     */
     void resetQueryCounter();
+
+    /**
+     * jumpNumber "jumps" the value of the stored number, mainNumber, by a
+     * specified amount. After a set number of "jumps", the JumpPrime will deactive.
+     * @param jumpValue the value (positive or negative) to "jump" the stored
+     * number by.
+     */
     void jumpNumber(int jumpValue);
 
 public:
+    /**
+     * Constructor for the JumpPrime object.
+     * @param initValue the initial positive integer encapsulated by the
+     * object. If none is provided, the defined default value will be used.
+     * @param jumpBound the number of times that the object with jump before
+     * becoming inactive. If none is specified, the defined default value
+     * will be used.
+     */
     JumpPrime(unsigned int initValue = DEFAULT_INITIAL_VALUE,
               unsigned int jumpBound = DEFAULT_JUMP_BOUND);
+
+    /**
+     * up returns the next highest prime number from the number stored in the
+     * JumpPrime object. Does not return accurate results if the JumpPrime object
+     * has been deactivated.
+     * PRECONDITION: the JumpPrime object is running.
+     * @return the next highest prime number. If the JumpPrime object has been
+     * deactivated, returns 0.
+     */
     unsigned int up();
+
+    /**
+     * down returns the next lowest prime number from the number stored in the
+     * JumpPrime object. Does not return accurate results if the JumpPrime
+     * object has been deactivated.
+     * PRECONDITION: the JumpPrime object is running.
+     * @return the next lowest prime number. If the JumpPrime object has been
+     * deactivated, returns 0.
+     * @return
+     */
     unsigned int down();
 
+    /**
+     * Reset attempts to reset the JumpPrime object to the original integer
+     * value. This will fail if the JumpPrime object was already made
+     * irreparable.
+     * PRECONDITION: the JumpPrime object is not permanently deactivated.
+     * @return true if the reset is successful, false otherwise.
+     */
     bool reset();
+
+    /**
+    * Revive attempts to revive a disabled JumpPrime object. This resets the
+    * query and jump count and allows the object to be queried again. If the
+    * object is already active, it permanently disables the object (makes it
+    * irreparable).
+    * PRECONDITION: the JumpPrime object is not currently active.
+    * @return true if the attempt to revive the object was successful. false
+    * otherwise.
+    */
     bool revive();
 
+    /**
+     * isActive returns whether or not the JumpPrime object has been deactivated
+     * due to excessive jumps.
+     * @return true if currently active, false otherwise
+     */
     bool isActive();
+
+    /**
+     * isDeactivated returns whether or not the JumpPrime object has been
+     * permanently deactivated due to improper operation.
+     * @return true if the JumpPrime object has been permanently disabled,
+     * false otherwise.
+     */
     bool isDisabled();
 
+    /**
+     * GetCurrentValue returns the current value encapsulated by the JumpPrime
+     * object. When the object is instantiated (or reset), this is the initial
+     * set value. After a jump, this is the new jumped-to value.
+     * @return the current value encapsulated by the JumpPrime object.
+     */
     unsigned int getCurrentValue();
-
-
-
 
 
 };
